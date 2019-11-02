0bf0f9ea1d5833d810d7569f793b1430b8f0e83e
Artyom Kazak <yom@artyom.me>
2019-11-02 15:22:32 +0300

    Optimization

--- default.nix
+++ default.nix
@@ -10,24 +10,26 @@ let
     fetchSubmodules = true;
   };
 
+  optimize = pkg: pkgs.haskell.lib.overrideCabal pkg (drv: {
+    buildFlags =
+      (drv.buildFlags or []) ++ [
+        "--ghcjs-option=-DGHCJS_BROWSER"
+        "--ghcjs-option=-O2"
+        "--ghcjs-option=-dedupe"
+      ];
+  });
+
   haskellPackages = reflex-platform.ghcjs.override {
     overrides = self: super: {
+      hpc = self.callHackage "hpc" "0.6.0.3" { };
       ghc-lib-parser = pkgs.haskell.lib.dontHaddock
         (super.callHackage "ghc-lib-parser" "8.8.1" { });
       ormolu = pkgs.haskell.lib.dontCheck
         (self.callCabal2nix "ormolu" "${ormolu-src}" { });
-      ormolu-live = pkgs.haskell.lib.dontCheck
-        (self.callCabal2nix "ormolu-live" ./pkg { });
-      hpc = self.callHackage "hpc" "0.6.0.3" { };
+      ormolu-live = optimize (pkgs.haskell.lib.dontCheck
+        (self.callCabal2nix "ormolu-live" ./pkg { }));
     };
   };
 
 in
   haskellPackages.ormolu-live
-
-  # ormolu = pkgs.haskell.lib.overrideCabal haskellPackages.ormolu
-  #   (drv: { buildFlags =
-  #             (drv.buildFlags or []) ++
-  #             ["--ghcjs-option=-O2 " "--ghcjs-option=-dedupe"];
-  #         }
-  #   );
